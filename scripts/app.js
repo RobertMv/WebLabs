e9860d8d60d202f62e28cd48f883a9947fc13c37
RobertMv <robert.mulyukov@gmail.com>
2022-01-21 20:18:14 +0300

    update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
 var main = function () {
     "use strict";
-    console.log("hello, world!");
+    var makeTabActive = function (tabNumber) {
+        //сконструируем селектор из tabNubmer
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        $(".tabs span").removeClass("active");
+        $(tabSelector).addClass("active");
+    };
+
+    $(".tabs a:nth-child(1)").on("click", function () {
+        makeTabActive(1);
+        return false;
+    });
+    $(".tabs a:nth-child(2)").on("click", function () {
+        makeTabActive(2);
+        return false;
+    });
+    $(".tabs a:nth-child(3)").on("click", function () {
+        makeTabActive(3);
+        return false;
+    });
 };
-$(document).ready(main);
